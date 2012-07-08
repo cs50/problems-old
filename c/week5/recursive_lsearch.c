ce9cbb81ac0df23512f4ed9f5a60da207c3aea87
JacksonSteinkamp <jharvard@appliance.(none)>
2012-07-08 16:39:33 -0400

    recursive implementation of linear search

--- c/week5/recursive_lsearch.c
+++ c/week5/recursive_lsearch.c
@@ -47,27 +47,29 @@ array_mem(int val_to_find, int *arr_front, int left)
 int
 main(void)
 {
-  // an arbitrary test array
-  int full_len = 3;
-  int arr[full_len];
-  arr[0] = 1;
-  arr[1] = 2;
-  arr[2] = 5;
+    // an arbitrary test array
+    int full_len = 3;
+    int arr[full_len];
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 5;
   
-  // pointer to the head of the full array
-  int *ptr = arr;
+    // pointer to the head of the full array
+    int *ptr = arr;
 
-  // check to see if '5' is in the array (it is, clearly)
-  if (array_mem(5, ptr, full_len))
-     printf("5 is in the array.\n");
-  else printf("5 isn't in the array.\n");
+    // check to see if '5' is in the array (it is, clearly)
+    if (array_mem(5, ptr, full_len))
+        printf("5 is in the array.\n");
+    else 
+        printf("5 isn't in the array.\n");
 
-  // check to see if '32' is in the array (it isn't).
-  if (array_mem(32, ptr, full_len))
-     printf("32 is in the array.\n");
-  else printf("32 isn't in the array.\n");
+    // check to see if '32' is in the array (it isn't).
+    if (array_mem(32, ptr, full_len))
+        printf("32 is in the array.\n");
+    else
+        printf("32 isn't in the array.\n");
 
-  return 0;
+    return 0;
 }
 
 /**********************************************************************
