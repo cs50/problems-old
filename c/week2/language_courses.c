90b6ad81725ea0d42fddb4e589a47c1cecdd58b7
Kevin Schmid <jharvard@appliance.localdomain>
2012-09-01 19:35:28 -0400

    i think this one could possibly spark a discussion about strategies for attacking hacker edition pset2, but it can be used in lesscomfortable-somewhereinbetween sections

---
+++ c/week2/language_courses.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<cs50.h>
+
+/*****************************************************
+* language_courses.c
+*
+* Kevin Schmid
+* Computer Science 50
+*
+* - Accepting user input
+* - Valdiating user input
+* - Idiomatic user input re-prompting
+* - Loops
+*
+* A particular university identifies its language courses
+* with a two character course identifier, in the following
+* pattern:
+* Aa, Ab, Ba, Bb, Ca, Cb ...
+* 0,   1,  2,  3,  4,  5 ...
+*
+* The courses are sequenced in this order.
+* Each course is a one semester course.
+*
+* Francis wants to write a program that will print out
+* the two character identifier for course n in the sequence.
+*
+* Write a program that gets an integer n from the user,
+* and implements this behavior.
+*
+* To be clear, course 0 corresponds to Aa, and course 4
+* corresponds to Ca.  Assume that the highest possible
+* course in the sequence is Zb.
+*
+* With that said, try to limit user input appropriately.
+* If the user provides out-of-range input, tell them to give you
+* another integer until they provide acceptable input.
+*
+* There are multiple ways to determine the correct
+* course identifier, but go for correctness first!
+*******************************************************/
+
+int
+main(void)
+{
+
+	// TODO: Tell the user about your need for an integer.
+	// TODO: Get an integer from the user.
+	// TODO: Make sure it's valid.  If it's not, ask them again.
+
+	// TODO: Determine the appropriate course identifier.
+	// TODO: Print it out.
+
+}
