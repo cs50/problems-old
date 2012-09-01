665978a5db9e0b23223e758be4ebc6681e09a1e2
Kevin Schmid <jharvard@appliance.localdomain>
2012-08-31 23:23:18 -0400

    stopwatch problem

---
+++ c/week1/stopwatch.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<cs50.h>
+
+/*****************************************************
+* stopwatch.c
+*
+* Kevin Schmid
+* Computer Science 50
+*
+* Fun with loops, sleep, carriage return, user input, and more!
+* Inspired by a question on the cs50-discuss google group in June 2012.
+* https://groups.google.com/d/msg/cs50-discuss/LRmxL7WCPQc/rLs7_UtDm1sJ
+*
+* Implement stopwatch.c according to the following specification:
+* The program should begin by asking the user for a number of seconds.
+* The number must not exceed 3600.
+* Then, countdown from the number of seconds the user
+* provided, printing the current time remaining in mm:ss format every second.
+*
+* After the program has counted down to 0 seconds, exit.
+*******************************************************/
+
+int
+main(void)
+{
+
+	// TODO: Tell the user about your need for a number of seconds
+	// TODO: Get number of seconds (restrictions?)
+
+	// TODO: Write an appropriate loop header
+
+	// TODO: Inside your loop, print the appropriate time in mm:ss format
+	// Make sure that you print the time in such a way that allows future "prints" to overwrite your current time
+	// You may need to use fflush
+
+	// TODO: Inside your loop, sleep for 1 second before continuing
+
+
+}
