2c66f062e9e27893a9bb8579425a3e320d87018a
Kevin Schmid <jharvard@appliance.localdomain>
2012-08-31 21:46:53 -0400

    i like this one :) - it's a problem on integer overflow

---
+++ c/week2/overflow.c
@@ -0,0 +1,45 @@
+/*****************************************************
+* overflow.c
+*
+* Kevin Schmid
+* Computer Science 50
+*
+* Covers loops and data types, with an accent
+* on integer overflow.
+*
+* Implement, in overflow.c, a program that behaves
+* in the following way:
+*
+* It accepts an integer from the user
+* (x below; assume this is a signed,
+* 32-bit integer) and prints the number of times that
+* x woud need to be incremented so that its value
+* is 0.  This should be greater than or equal to 0.
+*
+* Some things to think about:
+* 1) What data type(s) would be acceptable containers for
+     the value to be printed?
+  2) In a similar vein, what is the maximum value that should
+     ever be printed? Minimum?
+  3) Be creative in your implementation, but make
+     correctness and not speed the ultimate goal!
+*******************************************************/
+
+#include<stdio.h>
+#include<cs50.h>
+
+int
+main(void)
+{
+
+	printf("Please enter an integer: ");
+	int x = GetInt();
+
+	// TODO: Print a positive number representing the number of times
+	// x would need to be incremented to be equal to 0
+
+	// Make the assumptions stated in the specification.
+	// Why is this not the best thing to do in practice?
+
+
+}
