ad11b75c9ea2888c8a695de98f10df250541b255
Kevin Schmid <jharvard@appliance.localdomain>
2012-08-31 20:52:49 -0400

    echo problem

---
+++ c/week2/echo.c
@@ -0,0 +1,41 @@
+/*****************************************************
+* echo.c
+*
+* #argc #argv #loops #strcmp #flags #manpages
+*
+* Kevin Schmid
+* Computer Science 50
+*
+* echo is a Unix command that, according to its man page,
+* "display[s] a line of text."
+* For example:
+* francis@appliance:~$ echo This is CS50!
+* This is CS50!
+* francis@appliance:~$
+*
+* Below, try to implement your own version of the echo command.
+* - Don't worry about trailing whitespace, but do make sure that
+*   the user's string has spaces where appropriate.
+* - For simplicity, don't worry about flags.
+*
+* Possible extensions:
+* - Add some Easter eggs.  (Feel free to use this as a platform for self-expression.)
+* - Replace your Appliance installation's version of echo with your version of echo.
+	(How can you ensure that your version is the one being used :-)?)
+* - echo's output can be controlled by some flags, given as
+*   command line arguments:
+*     francis@appliance:~$ echo -n This is CS50 without the newline character!
+*     This is CS50 without the newline character!francis@appliance:~$
+*
+*   Try handling some or all of echo's flags.  (See the man page for a full list!)
+
+*******************************************************/
+
+#include<stdio.h>
+
+int
+main(int argc, char * argv[])
+{
+
+
+}
