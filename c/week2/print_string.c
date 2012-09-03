86f6377055463c8efdb6b8f1a91f5e4854ef3f5c
Kevin Schmid <jharvard@appliance.localdomain>
2012-09-02 22:00:24 -0400

    quick string problem

---
+++ c/week2/print_string.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<cs50.h>
+
+/*****************************************************
+ *
+ *	print_string.c
+ *  Kevin Schmid
+ *
+ *  Demonstrates the C representation of strings.
+ *
+ *  Implement the function print_string below.
+ *  print_string takes 1 string (char* argument) and
+ *  prints it to stdout.
+ *
+ *  Only use print_char in your implemntation.
+ *
+*******************************************************/
+
+/*
+ *	Prints a character, c, to stdout.
+ */
+void print_char(char c)
+{
+	printf("%c", c);
+}
+
+/*
+ *	Prints a string, s, to stdout.
+ *  Only uses print_char.
+ */
+void print_string(char* s)
+{
+
+}
+
+int main(void)
+{
+	char* s = GetString();
+	print_string(s);
+}
