e3e2f74f6330cc8ba551231e166687191162bbfd
Christopher Bartholomew <cbartholomew@gmail.com>
2012-09-30 14:27:51 -0400

    fixed instructor version comments and selection sort, added student version

--- arduino/week2/Practice/Student/WHY_RAINBOW_NO_WORK/WHY_RAINBOW_NO_WORK.ino
+++ arduino/week2/Practice/Student/WHY_RAINBOW_NO_WORK/WHY_RAINBOW_NO_WORK.ino
@@ -19,12 +19,9 @@
 #define SIZE 6
 
 // BONUS - pound define pin numbers to the actual colors, replace nums in pins w/ them
-#define RED    2
-#define ORANGE 4
-#define YELLOW 6
-#define GREEN  8
-#define BLUE   10
-#define INDIGO 12
+// NEEDED FOR BINARY SEARCH
+// #define RED    2
+
 
 void setup()
 {
@@ -33,34 +30,39 @@ void setup()
   int pins[SIZE] = {12,4,2,10,6,8};
   
   // TODO -  Enable pinMode() for each pin, going to OUTPUT
-  for(int i=0;i<=SIZE;i++)
-    pinMode(pins[i],OUTPUT);
   
   // TODO - show the rainbow, by passing in first parameter the array and the second parameter HIGH/LOW
-  rainbow(pins,HIGH);
+  rainbow(param1, param2);
   
   // TODO - turn off your rainbow
-  rainbow(pins,LOW);
+  rainbow(param1, param2);
   
-  // TODO - Use one of the three methods to Sort the Rainbow
-  //bubble_sort(pins, SIZE);
-  //selection_sort(pins, SIZE);
-  merge_sort(pins, SIZE);
+  // TODO - pick one of the three methods to implment!
+  // bubble_sort(pins, SIZE);
+  // http://en.wikipedia.org/wiki/Bubble_sort
   
-  // anticpiation
+  // selection_sort(pins, SIZE);
+  // http://en.wikipedia.org/wiki/Selection_sort
+  
+  // merge_sort(pins, SIZE);
+  //http://en.wikipedia.org/wiki/Merge_sort
+  
+  
+  // create anticpiation
   delay(2000);
   
-  // TODO - is the rainbow fixed?
-  rainbow(pins, HIGH);
+  // TODO - light our rainbow up again by calling its function with the correct param
+  rainbow(param1, param2);
   
-  rainbow(pins, LOW);
-  // anticpiation
+  // TODO - turn off our rainbow to prepare for binary search
+  rainbow(param1, param2);
+  
+  // create anticpiation
   delay(2000);
   
   // BONUS - implement pick_my_color(COLOR,RAINBOW,SIZE) using binary search
-  // Remeber! Binary Search will need a "Sorted" array!
-  // http://en.wikipedia.org/wiki/Binary_search_algorithm
-  pick_my_color(BLUE,pins,SIZE);
+  // Remeber! Binary Search will need a "Sorted" array! Pass in a COLOR as a parameter
+  // pick_my_color(BLUE,pins,SIZE);
  
  }
 
@@ -69,15 +71,14 @@ void rainbow(int *pins, int power)
   // TODO - turn on the Broken Rainbow, using digitalWrite - for each pin - setting it to high
   for(int i=0;i<=SIZE;i++) 
   {   
-    // TODO - Turn on each LED in the rainbow array
-      digitalWrite(pins[i], power); 
+     // TODO - Turn on each LED in the rainbow array ( digitalWrite(x,y) )
+     
     
-      // TODO if power is HIGH - delay for 3 seonds
-      if(power==HIGH)
-        delay(1000);
-    
+     // TODO if power is HIGH - delay for 1 seonds    
   }
+  
 }
+
 /* bubble_sort(int*, int)
  * @param1: a reference to pins
  * @param2: size of array
@@ -93,14 +94,8 @@ void bubble_sort(int *pins, int size)
         // focus on the first size - i values (-1 to accommodate array[j + 1])
         for (int j = 0; j < size - i - 1; j++)
         {
-            // swap if the next element in the array is smaller
-            if (pins[j] > pins[j + 1])
-            {
-                int temp = pins[j];
-                pins[j]  = pins[j + 1];
-                pins[j + 1] = temp;
-            }
-        }
+          // TODO
+        }  
     }
 }
 
@@ -112,25 +107,16 @@ void bubble_sort(int *pins, int size)
  */
 void selection_sort(int *pins, int size)
 {
-   // TODO - Selection Sort
-    // sort ALL the numberz!
+    // TODO - Selection Sort
     for (int i = 0; i < size; i++)
     {
         int min = i;
-
+        // TODO
         // find the index of the minimum value in the array
-        for (int j = i; j < size; j++)
-            if (array[j] < array[min])
-                min = j;
 
         // if the index of the min value is not i, swap so that the min value
         // goes into the array at index i.
-        if (min != i)
-        {
-            int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-        }
+
     }
 }
 /* merge_sort(int*, int)
@@ -142,46 +128,28 @@ void selection_sort(int *pins, int size)
 void merge_sort(int* pins, int size)
 {
     // base case! array is guaranteed to be sorted
-    if (size < 2)
-        return;
 
     // split the list and create copies of each half
-    int mid = size / 2;
-
-    int left[mid];
-    for (int i = 0; i < mid; i++)
-        left[i] = pins[i];
 
     // make sure you start this for loop at 0 too, not at mid!
-    int right[size - mid];
-    for (int i = 0; i < size - mid; i++)
-        right[i] = pins[mid + i];
 
-    // recurrecurrecur...sionsionsion!
-    merge_sort(left, mid);
-    merge_sort(right, size - mid);
+    // Some recursion goes here, BTW
+    // merge_sort left 
+    
+    // merge_sort right
 
-    // merge the lists back into array
+  
+    // reset the index to zero for each half
     int left_index = 0, right_index = 0;
+    
+    // create a for loop to merge the lists back into array
     for (int i = 0; i < size; i++)
     {
         // be careful to not index out of bounds!
-        if (left_index < mid && right_index < size - mid)
-        {
-            if (left[left_index] <= right[right_index])
-                pins[i] = left[left_index++];
-            else
-                pins[i] = right[right_index++];
-        }
-        else if (left_index >= mid)
-        {
-            pins[i] = right[right_index++];
-        }
-        else
-        {
-            pins[i] = left[left_index++];
-        }
+        
+
     }
+    
 }
 
 /* pick_my_color(int,int*,size)
@@ -205,17 +173,14 @@ void pick_my_color(int color, int* rainbow, int size)
         // http://googleresearch.blogspot.com/2006/06/extra-extra-read-all-about-it-nearly.html
         int mid = ((unsigned int)low + (unsigned int)high) >> 1;
 
-        // return if we've found a match!
-        if (rainbow[mid] == color)
-            return digitalWrite(color,HIGH);
+        // turn on the selected light if we've found a match
+
 
         // gotta look in the upper half of the array
-        else if (rainbow[mid] < color)
-            low = mid + 1;
+
 
         // gotta look in the lower half of the array
-        else
-            high = mid - 1;
+   
     }
 }
 
